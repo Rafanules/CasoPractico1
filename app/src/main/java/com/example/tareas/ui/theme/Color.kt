0dd9640c6e0bacccba46393e89e786b97d04ada1
rafvilcli <rafvilcli@alu.edu.gva.es>
2023-10-24 12:19:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tareas/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.tareas.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFFFEB3B)
+val PurpleGrey80 = Color(0xFFFFC107)
+val Pink80 = Color(0xFFFF9800)
+
+val Purple40 = Color(0xFF8BC34A)
+val PurpleGrey40 = Color(0xFF4CAF50)
+val Pink40 = Color(0xFF009688)
